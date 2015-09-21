msft-mirror-aosp/platform.external.vogar

commit 128163bc9658aad4a0b331ff9a777a7719d8c459

Make vogar work if one uses OUT_DIR
Change-Id: I19824d792d5dcbed32846912fb36654894fcc0ac

src/vogar/android/AndroidSdk.java
@@ -105,10 +105,10 @@ public class AndroidSdk {
          *  <sdk>/platforms/android-?/android.jar
          *
          * Android build tree (target):
-         *  <source>/out/host/linux-x86/bin/aapt
-         *  <source>/out/host/linux-x86/bin/adb
-         *  <source>/out/host/linux-x86/bin/dx
-         *  <source>/out/target/common/obj/JAVA_LIBRARIES/core-libart_intermediates/classes.jar
+         *  <ANDROID_HOST_OUT>/bin/aapt
+         *  <ANDROID_HOST_OUT>bin/adb
+         *  <ANDROID_HOST_OUT>/bin/dx
+         *  <OUT_DIR>/target/common/obj/JAVA_LIBRARIES/core-libart_intermediates/classes.jar
          */
 
         if ("platform-tools".equals(parentFileName)) {
@@ -122,9 +122,15 @@ public class AndroidSdk {
                     .getParentFile().getParentFile().getParentFile();
             log.verbose("using android build tree: " + sourceRoot);
 
-            String pattern = "out/target/common/obj/JAVA_LIBRARIES/%s_intermediates/classes.jar";
+            String outDir = System.getenv("OUT_DIR");
+            if (outDir == null || outDir.length() == 0) {
+              outDir = "./out";
+            } else {
+              outDir += "/";
+            }
+            String pattern = outDir + "target/common/obj/JAVA_LIBRARIES/%s_intermediates/classes.jar";
             if (modeId.isHost()) {
-                pattern = "out/host/common/obj/JAVA_LIBRARIES/%s_intermediates/classes"
+                pattern = outDir + "host/common/obj/JAVA_LIBRARIES/%s_intermediates/classes"
                         + ((useJack) ? ".jack" : ".jar");
             }
 

bin/vogar-android
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-vogar_jar=${ANDROID_BUILD_TOP}/out/target/common/obj/JAVA_LIBRARIES/vogar.jar_intermediates/javalib.jar
+vogar_jar=${OUT_DIR-${ANDROID_BUILD_TOP}/out}/target/common/obj/JAVA_LIBRARIES/vogar.jar_intermediates/javalib.jar
 exec java -classpath ${vogar_jar} vogar.Vogar "$@"

src/vogar/android/HostRuntime.java
@@ -84,15 +84,22 @@ public final class HostRuntime implements Mode {
     }
 
     @Override public VmCommandBuilder newVmCommandBuilder(Action action, File workingDirectory) {
-        String buildRoot = System.getenv("ANDROID_BUILD_TOP");
+        String hostOut = System.getenv("ANDROID_HOST_OUT");
+        if (hostOut == null || hostOut.length() == 0) {
+          hostOut = System.getenv("ANDROID_BUILD_TOP");
+          if (hostOut == null) {
+            hostOut = "";
+          }
+          hostOut += "out/host/linux-x86";
+        }
 
         List<File> jars = new ArrayList<File>();
         for (String jar : modeId.getJarNames()) {
-            jars.add(new File(buildRoot, "out/host/linux-x86/framework/" + jar + ".jar"));
+            jars.add(new File(hostOut, "framework/" + jar + ".jar"));
         }
         Classpath bootClasspath = Classpath.of(jars);
 
-        String libDir = buildRoot + "/out/host/linux-x86";
+        String libDir = hostOut;
         if (variant == Variant.X32) {
             libDir += "/lib";
         } else if (variant == Variant.X64) {
@@ -106,13 +113,13 @@ public final class HostRuntime implements Mode {
         vmCommand.add("ANDROID_PRINTF_LOG=tag");
         vmCommand.add("ANDROID_LOG_TAGS=*:i");
         vmCommand.add("ANDROID_DATA=" + dalvikCache().getParent());
-        vmCommand.add("ANDROID_ROOT=" + buildRoot + "/out/host/linux-x86");
+        vmCommand.add("ANDROID_ROOT=" + hostOut);
         vmCommand.add("LD_LIBRARY_PATH=" + libDir);
         vmCommand.add("DYLD_LIBRARY_PATH=" + libDir);
         // This is needed on the host so that the linker loads core.oat at the necessary address.
         vmCommand.add("LD_USE_LOAD_BIAS=1");
         Iterables.addAll(vmCommand, run.invokeWith());
-        vmCommand.add(buildRoot + "/out/host/linux-x86/bin/" + run.vmCommand);
+        vmCommand.add(hostOut + "/bin/" + run.vmCommand);
 
         VmCommandBuilder builder = new VmCommandBuilder(run.log);
 

commit 83909fb7735168d513557d6433e76ae6ffd7ea26

Fix vogar when OUT_DIR is not set
Commit 128163bc broke it. Commit af11af97e was a follow up
to fix host but did not fix it for target.

Change-Id: Ibd206117a42628d52a137d12c94527f1e9e5dcb0

src/vogar/android/AndroidSdk.java
@@ -124,7 +124,7 @@ public class AndroidSdk {
 
             String outDir = System.getenv("OUT_DIR");
             if (outDir == null || outDir.length() == 0) {
-              outDir = "./out";
+              outDir = "./out/";
             } else {
               outDir += "/";
             }