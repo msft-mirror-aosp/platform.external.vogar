ec65c980a13b3e6eb0bd78662f330c1de9988420
Sundong Ahn <sundongahn@google.com>
2018-11-08 19:01:14 +0900

    Change the impl lib name of java_sdk_library
    The impl lib name of java_sdk_library is changed to {module_name}.jar
    instead of {module_name}.impl.jar
    
    Bug: 110404779
    Test: m -j
    Change-Id: I60a7890a037968788074ca1c3642d515169cd08a

--- src/vogar/ModeId.java
+++ src/vogar/ModeId.java
@@ -83,7 +83,7 @@ public enum ModeId {
             "voip-common",
             "ims-common",
             "framework-oahl-backward-compatibility",
-            "android.test.base.impl",
+            "android.test.base",
             // TODO: get this list programatically
     };
 
