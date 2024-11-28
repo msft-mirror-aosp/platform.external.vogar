0b1290c387c87e8843292268fc918e49a68edd93
David Srbecky <dsrbecky@google.com>
2024-11-28 16:49:56 +0000

    Vogar: Remove desugar completely
    It is not needed since I4419f5b67bc2276b96044acd05028392d76101e0
    
    Test: art/tools/run-libcore-tests.sh --mode host
    Change-Id: I55433e5feaf043f91ba05a5456c91b1aa3298ba7

--- src/vogar/android/AndroidSdk.java
+++ src/vogar/android/AndroidSdk.java
@@ -54,7 +54,6 @@ public class AndroidSdk {
     private final Mkdir mkdir;
     private final File[] compilationClasspath;
     private final String androidJarPath;
-    private final String desugarJarPath;
     private final Md5Cache dexCache;
     private final Language language;
     private final boolean serialDexing;
@@ -101,14 +100,12 @@ public class AndroidSdk {
          *  ${ANDROID_BUILD_TOP}/out/host/linux-x86/bin/aapt
          *  ${ANDROID_BUILD_TOP}/out/host/linux-x86/bin/adb
          *  ${ANDROID_BUILD_TOP}/out/host/linux-x86/bin/d8
-         *  ${ANDROID_BUILD_TOP}/out/host/linux-x86/bin/desugar.jar
          *  ${ANDROID_BUILD_TOP}/out/target/common/obj/JAVA_LIBRARIES/core-libart_intermediates
          *      /classes.jar
          */
 
         File[] compilationClasspath;
         String androidJarPath;
-        String desugarJarPath = null;
 
         // Accept that we are running in an SDK if the user has added the build-tools or
         // platform-tools to their path.
@@ -126,13 +123,6 @@ public class AndroidSdk {
             androidJarPath = new File(newestPlatform.getAbsolutePath(), "android.jar")
                     .getAbsolutePath();
             log.verbose("using android sdk: " + sdkRoot);
-
-            // There must be a desugar.jar in the build tool directory.
-            desugarJarPath = buildToolDirString + "/desugar.jar";
-            File desugarJarFile = new File(desugarJarPath);
-            if (!desugarJarFile.exists()) {
-                throw new RuntimeException("Could not find " + desugarJarPath);
-            }
         } else if ("bin".equals(buildToolDirString)) {
             log.verbose("Using android source build mode to find dependencies.");
             String tmpJarPath = "prebuilts/sdk/current/public/android.jar";
@@ -167,16 +157,6 @@ public class AndroidSdk {
                 log.warn("ANDROID_HOST_OUT not set. Assuming linux-x86");
                 hostOutDir = outDir + "/host/linux-x86";
             }
-
-            String desugarPattern =
-                androidBuildTop + "/prebuilts/build-tools/common/framework/desugar.jar";
-            File desugarJar = new File(desugarPattern);
-
-            if (!desugarJar.exists()) {
-                throw new RuntimeException("Could not find " + desugarPattern);
-            }
-
-            desugarJarPath = desugarJar.getPath();
 
             if (!supportBuildFromSource) {
                 compilationClasspath = new File[]{};
@@ -221,7 +201,7 @@ public class AndroidSdk {
                     + ARBITRARY_BUILD_TOOL_NAME);
         }
 
-        return new AndroidSdk(log, mkdir, compilationClasspath, androidJarPath, desugarJarPath,
+        return new AndroidSdk(log, mkdir, compilationClasspath, androidJarPath,
                 new HostFileCache(log, mkdir), language, serialDexing, verboseDexStats);
     }
 
@@ -263,13 +243,12 @@ public class AndroidSdk {
 
     @VisibleForTesting
     AndroidSdk(Log log, Mkdir mkdir, File[] compilationClasspath, String androidJarPath,
-               String desugarJarPath, HostFileCache hostFileCache, Language language,
+               HostFileCache hostFileCache, Language language,
                boolean serialDexing, boolean verboseDexStats) {
         this.log = log;
         this.mkdir = mkdir;
         this.compilationClasspath = compilationClasspath;
         this.androidJarPath = androidJarPath;
-        this.desugarJarPath = desugarJarPath;
         this.dexCache = new Md5Cache(log, "dex", hostFileCache);
         this.language = language;
         this.serialDexing = serialDexing;
