a242522d91adc9918ada206de1241eb65363a302
Adrian Roos <roosa@google.com>
2025-03-12 14:23:48 +0100

    external/vogar: remove usages of deprecated org.mockito.runners
    The MockitoJUnitRunner was moved to org.mockito.junit, and
    the deprecated version in the old package has been removed
    upstream. Remove remaining usages to unblock the upgrade.
    
    Test: m checkbuild
    Flag: TEST_ONLY
    Bug: 310268946
    Change-Id: I3b3bcd9f3d97e519caf0479f043356dc872956f8

--- test/vogar/android/DeviceRuntimeAdbTargetTest.java
+++ test/vogar/android/DeviceRuntimeAdbTargetTest.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import vogar.Classpath;
 import vogar.Mode;
 import vogar.ModeId;
